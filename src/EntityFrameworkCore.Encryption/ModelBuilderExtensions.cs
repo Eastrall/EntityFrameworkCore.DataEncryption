bf90510089edf34b73000a646ed01f2c561b0d60
Filipe GP <eastrall@users.noreply.github.com>
2019-03-07 09:02:11 +0100

    Add Encryption converter

--- src/EntityFrameworkCore.Encryption/ModelBuilderExtensions.cs
+++ src/EntityFrameworkCore.Encryption/ModelBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore.Encryption.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,17 +10,33 @@ namespace Microsoft.EntityFrameworkCore.Encryption
 {
     public static class ModelBuilderExtensions
     {
-        public static void UseEncryption(this ModelBuilder modelBuilder)
+        public static void UseEncryption(this ModelBuilder modelBuilder, IEncryptionProvider encryptionProvider)
         {
+            if (encryptionProvider == null)
+                return;
+
+            var encryptionConverter = new EncryptionConverter(encryptionProvider);
+
             foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
             {
                 foreach (IMutableProperty property in entityType.GetProperties())
                 {
-                    object[] attributes = property.PropertyInfo.GetCustomAttributes(typeof(EncryptedAttribute), false);
-                    
-                    // TODO: set value converter here
+                    if (property.ClrType == typeof(string))
+                    {
+                        object[] attributes = property.PropertyInfo.GetCustomAttributes(typeof(EncryptedAttribute), false);
+
+                        if (attributes.Any())
+                            property.SetValueConverter(encryptionConverter);
+                    }
                 }
             }
         }
+    }
+
+    public interface IEncryptionProvider
+    {
+        string Encrypt(string dataToEncrypt);
+
+        string Decrypt(string dataToDecrypt);
     }
 }
