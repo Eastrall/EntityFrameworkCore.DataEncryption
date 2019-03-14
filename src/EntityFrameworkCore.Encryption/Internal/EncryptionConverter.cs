4492727aa5e3e48fbc141f0b7e9a6a88993faec9
Filipe GP <eastrall@users.noreply.github.com>
2019-03-14 13:29:32 +0100

    Remove magic number and add more documentation.

--- src/EntityFrameworkCore.Encryption/Internal/EncryptionConverter.cs
+++ src/EntityFrameworkCore.Encryption/Internal/EncryptionConverter.cs
@@ -2,8 +2,16 @@
 
 namespace Microsoft.EntityFrameworkCore.Encryption.Internal
 {
+    /// <summary>
+    /// Defines the internal encryption converter for string values.
+    /// </summary>
     internal sealed class EncryptionConverter : ValueConverter<string, string>
     {
+        /// <summary>
+        /// Creates a new <see cref="EncryptionConverter"/> instance.
+        /// </summary>
+        /// <param name="encryptionProvider">Encryption provider</param>
+        /// <param name="mappingHints">Entity Framework mapping hints</param>
         public EncryptionConverter(IEncryptionProvider encryptionProvider, ConverterMappingHints mappingHints = null) 
             : base(x => encryptionProvider.Encrypt(x), x => encryptionProvider.Decrypt(x), mappingHints)
         {
