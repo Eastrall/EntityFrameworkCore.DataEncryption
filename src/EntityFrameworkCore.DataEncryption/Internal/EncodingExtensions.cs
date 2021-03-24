c1d166f41688203894dcabfa3011c175e0c922c7
Richard Deeming <RichardD2@users.noreply.github.com>
2021-03-24 17:59:12 +0000

    Added support for SecureString and byte array model properties; added support for storing values in binary as well as Base64-encoded strings; added class to migrate from one provider to another; restored fixed IV option for AesProvider.

---
+++ src/EntityFrameworkCore.DataEncryption/Internal/EncodingExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Microsoft.EntityFrameworkCore.DataEncryption.Internal
+{
+	internal static class EncodingExtensions
+	{
+		public static byte[] GetBytes(this Encoding encoding, SecureString value)
+		{
+			if (encoding is null) throw new ArgumentNullException(nameof(encoding));
+			if (value is null || value.Length == 0) return Array.Empty<byte>();
+
+			IntPtr valuePtr = IntPtr.Zero;
+			try
+			{
+				valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+				if (valuePtr == IntPtr.Zero) return Array.Empty<byte>();
+
+				unsafe
+				{
+					char* chars = (char*)valuePtr;
+					Debug.Assert(chars != null);
+
+					int byteCount = encoding.GetByteCount(chars, value.Length);
+
+					var result = new byte[byteCount];
+					fixed (byte* bytes = result)
+					{
+						encoding.GetBytes(chars, value.Length, bytes, byteCount);
+					}
+
+					return result;
+				}
+			}
+			finally
+			{
+				if (valuePtr != IntPtr.Zero)
+				{
+					Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+				}
+			}
+		}
+
+		public static SecureString GetSecureString(this Encoding encoding, byte[] bytes)
+		{
+			if (encoding is null) throw new ArgumentNullException(nameof(encoding));
+			if (bytes is null || bytes.Length == 0) return default;
+
+			unsafe
+			{
+				fixed (byte* pB = bytes)
+				{
+					int charCount = encoding.GetCharCount(pB, bytes.Length);
+					Span<char> chars = charCount < 1024 ? stackalloc char[charCount] : new char[charCount];
+					fixed (char* pC = &MemoryMarshal.GetReference(chars))
+					{
+						charCount = encoding.GetChars(pB, bytes.Length, pC, chars.Length);
+						return new SecureString(pC, charCount);
+					}
+				}
+			}
+		}
+	}
+}
